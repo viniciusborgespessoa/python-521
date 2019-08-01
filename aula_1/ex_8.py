d52d7abdd9bcd7212297cd2d894d6a84b9e76870
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-08-01 16:30:22 -0300

    primeiro commit

--- aula_1/ex_8.py
+++ aula_1/ex_8.py
@@ -1,4 +1,7 @@
+import sys
+import os
 import flask
+
 
 app = flask.Flask(__name__)
 
@@ -10,5 +13,10 @@ def get_index():
 
 if __name__ == '__main__':
 
+	current_module = os.path.abspath(os.path.curdir)
+	sys.path.append(current_module)
 
-	app.run(debug=True)
+	os.environ['FLASK_ENV'] = 'development'
+	os.environ['FLASK_APP'] = 'ex_8.py'
+	
+	app.run(host='0.0.0.0', debug=True)
