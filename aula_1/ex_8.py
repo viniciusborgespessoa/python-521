6d5745df1d24ccc83843192082f5aef5f5455c88
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-08-01 16:58:45 -0300

    primeiro commit

--- aula_1/ex_8.py
+++ aula_1/ex_8.py
@@ -8,8 +8,20 @@ app = flask.Flask(__name__)
 @app.route('/')
 
 def get_index():
-	return 'hello, world'
+	return 'hello, world2'
 
+
+@app.route('/users', methods=[ 'GET'])
+def get_users():
+	return 'users'
+
+@app.route('/users', methods=[ 'POST'])
+def post_users():
+	return 'post users'
+
+@app.route('/users/<int:user_id>', methods=[ 'GET'])
+def get_users_by_id(user_id):
+	return 'get users ' + str(user_id)
 
 if __name__ == '__main__':
 
@@ -18,5 +30,5 @@ if __name__ == '__main__':
 
 	os.environ['FLASK_ENV'] = 'development'
 	os.environ['FLASK_APP'] = 'ex_8.py'
-	
+
 	app.run(host='0.0.0.0', debug=True)
